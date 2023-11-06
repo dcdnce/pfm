3c052fdd0f14b2ae97d5c6e442a81091d0929404
dcdnce <pforesti@student.42nice.fr>
2023-11-06 13:13:53 +0100

    ✨ lookRotation

--- core/transform/matrix_transform.hpp
+++ core/transform/matrix_transform.hpp
@@ -14,6 +14,9 @@ namespace pfm {
     PFM_FUNC_DECL mat<4, 4, T> lookAt(vec<3, T> const& eye, vec<3, T> const& target, vec<3, T> const& up);
 
     template<typename T>
+    PFM_FUNC_DECL mat<4, 4, T> lookRotation(vec<3, T> const& forward, vec<3, T> const& up = vec3(0.f, 1.f, 0.f));
+
+    template<typename T>
     PFM_FUNC_DECL mat<4, 4, T> perspective(T const& fovy, T const& ar,  T const& znear, T const& zfar);
 
     template<typename T>
